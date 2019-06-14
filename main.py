6f6e2616ba2870602c02ace840eb8a082a3df7ac
Alex B <WeirdAlPal@users.noreply.github.com>
2019-06-14 00:31:44 -0500

    Web

--- main.py
+++ main.py
@@ -5,16 +5,43 @@ app =Flask(__name__)
 app.config['DEBUG'] = True
 
 
-@app.route("/")
-def index():
-    return render_template('encrypt_form.html')
+form = """"
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+    </body>
+</html>
+""""
 
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
     text = request.form['text']
-    encrypted_text = rotate_string(text, rot)
-return '<h1>' + encrypted_text + '</h1>'
+    encrypted_string = rotate_string(text, rot)
+return form.format(encryped_string)
+
+@app.route("/")
+def index():
+    return form.format("")
 
 app.run()
